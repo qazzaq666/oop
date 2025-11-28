2abd6427cb6eb6638d3036a64a57e73ec4ae9141
qazzaq666 <vodka.seledka112@gmail.com>
2025-11-28 19:00:32 +0300

    123

---
+++ tasks/LAB/lab2/1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void allocArr(int*& p, int n) {
+    p = new int[n];
+}
+
+void reverseArr(int* arr, int n) {
+    int i = 0;
+    int j = n - 1;
+    while (i < j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+        i++;
+        j--;
+    }
+}
+
+int count_words(const char* src) {
+    int c = 0;
+    bool in = false;
+    for (int i = 0; src[i] != '\0'; i++) {
+        if (src[i] != ' ' && src[i] != '\t' && src[i] != '\n') {
+            if (!in) {
+                c++;
+                in = true;
+            }
+        } else {
+            in = false;
+        }
+    }
+    return c;
+}
+
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    cout << arr << endl;
+    cout << &arr[0] << endl;
+    cout << *arr << endl;
+    cout << arr[0] << endl;
+
+    int* p = arr;
+    for (int i = 0; i < 5; i++) {
+        *(p + i) += 10;
+    }
+
+    int* dyn = nullptr;
+    allocArr(dyn, 5);
+    for (int i = 0; i < 5; i++) dyn[i] = i + 1;
+
+    int i = 0;
+    int* q = dyn;
+    while (i < 5) {
+        cout << *(q + i) << " ";
+        i++;
+    }
+    cout << endl;
+
+    reverseArr(dyn, 5);
+    for (int k = 0; k < 5; k++) cout << dyn[k] << " ";
+    cout << endl;
+
+    const char* s = "Я ебал в рот ходить на пары";
+    cout << count_words(s) << endl;
+    cout << s << endl;
+
+    delete[] dyn;
+    return 0;
+}
+
+
+
