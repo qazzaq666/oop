98368428dea7cec100a43622cedec623ad6cdb0a
qazzaq666 <vodka.seledka112@gmail.com>
2026-03-06 16:03:59 +0300

    cmake pz test

---
+++ 4sem/pz2/build/CMakeFiles/pz2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pz2.dir/main.cpp.o"
+  "CMakeFiles/pz2.dir/main.cpp.o.d"
+  "CMakeFiles/pz2.dir/src/triangle.cpp.o"
+  "CMakeFiles/pz2.dir/src/triangle.cpp.o.d"
+  "pz2"
+  "pz2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pz2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4sem/pz2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "src/triangle.cpp"
+#include "src/Triangle.hpp"
+using namespace std;
+int main() {
+    cout<<2;
+    return 0;
+}
