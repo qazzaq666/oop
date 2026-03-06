3c8f3ec4eeaaa4a482067f4d668c4ee26ef18e53
qazzaq666 <vodka.seledka112@gmail.com>
2026-03-06 15:41:21 +0300

    pz

---
+++ 4sem/pz2/Triangle.hpp
@@ -0,0 +1,12 @@
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+class Triangle {
+    private:
+        double l1, l2, l3;
+    public:
+        Triangle(double a1, double a2, double a3);
+        double getsquare();
+};
+
+#endif // TRIANGLE_HPP
