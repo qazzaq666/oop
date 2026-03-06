8a96bcb9f7087ef555007db2234a702e584949b9
qazzaq666 <vodka.seledka112@gmail.com>
2026-03-06 16:26:06 +0300

    rearrangement

--- 4sem/pz2/src/triangle.cpp
+++ 4sem/pz2/src/triangle.cpp
@@ -1,12 +1,12 @@
 #include "Triangle.hpp"
 #include <cmath>
-// Triangle::Triangle(double a1, double a2, double a3) {
-//     l1 = a1;
-//     l2 = a2;
-//     l3 = a3;
-// }
+Triangle::Triangle(double a1, double a2, double a3) {
+    l1 = a1;
+    l2 = a2;
+    l3 = a3;
+}
 
-// double Triangle::getsquare() {
-//     double s = (l1 + l2 + l3) / 2;
-//     return sqrt(s * (s - l1) * (s - l2) * (s - l3));
-// }
+double Triangle::getsquare() {
+    double s = (l1 + l2 + l3) / 2;
+    return sqrt(s * (s - l1) * (s - l2) * (s - l3));
+}
